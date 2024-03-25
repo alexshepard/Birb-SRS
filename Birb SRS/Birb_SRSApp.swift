a4ab317431c14ef58a27a1756e44af473e28a396
Alex Shepard <alex@meatfreezer.com>
2024-03-24 17:37:11 -0700

    Initial Commit

---
+++ Birb SRS/Birb_SRSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Birb_SRSApp.swift
+//  Birb SRS
+//
+//  Created by Alex Shepard on 3/24/24.
+//
+
+import SwiftUI
+
+@main
+struct Birb_SRSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
